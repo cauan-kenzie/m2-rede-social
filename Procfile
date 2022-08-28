f2c396edd4be3bfc2d8d0713ef327fbb5c6c3168
kenzie-cauan <cauan@kenzie.com.br>
2022-08-28 18:47:06 -0300

    procfile added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn _project.wsgi -b :8000 --workers 3
--- _project/settings.py
+++ _project/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['m2-rede-social.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    "m2-rede-social.herokuapp.com",
+    "localhost",
+]
 
 
 # Application definition
